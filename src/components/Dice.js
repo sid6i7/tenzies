35ca3f3b9a1137c69f163e15bcc77c66c6471071
Siddhant Jain <siddhantjain209@gmail.com>
2024-05-09 07:47:35 +0530

    add game finish logic, lock dice, roll dice and reset game

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,9 +1,12 @@
-import css from '../css/Dice.css';
+import css from "../css/Dice.css";
 
 export const Dice = (props) => {
-    return (
-        <div className={`dice ${props.locked && 'green-bg'}`}>
-            <h1 className='dice--num'>{props.num}</h1>
-        </div>
-    )
-}
+  return (
+    <div
+      className={`dice ${props.locked && "green-bg"}`}
+      onClick={props.toggleDiceLock}
+    >
+      <h1 className="dice--num">{props.num}</h1>
+    </div>
+  );
+};
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,38 +1,92 @@
 import css from "../css/Game.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import { Dice } from "./Dice";
+import Confetti from "react-confetti";
 
 export const Game = () => {
 
-    const initDice = () => {
-        const dices = [];
-        for(let i = 0; i<10; i++) {
-            const num = Math.floor((Math.random() * 6)+1);
-            dices.push({
-                num: num,
-                locked: false
-            });
-        }
-        console.log(dices);
-        return dices;
-    }
+  const getRandomNum = () => Math.floor(Math.random() * 6 + 1);
 
-    const [dices, setDices] = useState(initDice());
-    const [isFinished, setIsFinished] = useState(false);
+  const initDices = () => {
+    const dices = [];
+    for (let i = 0; i < 10; i++) {
+      const num = getRandomNum();
+      dices.push({
+        id: i,
+        num: num,
+        locked: false,
+      });
+    }
+    console.log(dices);
+    return dices;
+  };
+
+  const toggleDiceLock = (id) => {
+    setDices((oldDices) =>
+      oldDices.map((dice) => {
+        if (dice.id === id) {
+          return {
+            ...dice,
+            locked: !dice.locked,
+          };
+        } else return dice;
+      })
+    );
+  };
+
+  const rollDice = () => {
+    setDices((oldDices) => {
+      return oldDices.map((dice) => {
+        if (!dice.locked) {
+          return {
+            ...dice,
+            num: getRandomNum(),
+          };
+        } else {
+          return dice;
+        }
+      });
+    });
+  };
+
+  const resetGame = () => {
+    setDices(initDices());
+    setIsFinished(false);
+  };
+
+  const [isFinished, setIsFinished] = useState(false);
+  const [dices, setDices] = useState(() => initDices());
+
+  useEffect(() => {
+    const checkArr = dices.filter((dice) => dice.num === dices[0].num);
+    setIsFinished(checkArr.length === dices.length);
+  }, [dices]);
 
   return (
-    <div id='game'>
-      <h1 id='game--title'>Tenzies</h1>
-      <h3 id='game--rule'>
+    <div id="game">
+      {isFinished && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+      <h1 id="game--title">Tenzies</h1>
+      <h3 id="game--rule">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </h3>
-      <div id='dice-container'>
-      {dices.map((dice, idx) => {
-        return <Dice key={idx} num={dice.num} locked={dice.locked}/>
-      })}
+      <div id="dice-container">
+        {dices.map((dice, idx) => {
+          return (
+            <Dice
+              key={idx}
+              num={dice.num}
+              locked={dice.locked}
+              toggleDiceLock={() => toggleDiceLock(dice.id)}
+            />
+          );
+        })}
       </div>
-      <button id='game--btn'>{isFinished ? 'Reset' : 'Roll'}</button>
+      <button id="game--btn" onClick={isFinished ? resetGame : rollDice}>
+        {isFinished ? "Reset" : "Roll"}
+      </button>
     </div>
   );
 };
